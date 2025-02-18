163594a4972aef2d601f83570630968c550dd84f
Elvira Latypova <ellalatypova666@gmail.com>
2025-02-18 16:43:20 +0500

    Update styles

--- src/components/shared/bento-card.tsx
+++ src/components/shared/bento-card.tsx
@@ -39,6 +39,7 @@ export const BentoCard: React.FC<BentoCardProps> = ({
         src={src}
         loading="lazy"
         className="absolute left-0 top-0 size-full object-cover object-center"
+        alt=""
       />
       <div className="relative z-10 flex size-full flex-col justify-between p-5">
         <div>
